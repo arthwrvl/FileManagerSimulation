33e2098fbfd56ce7c017502efd3d78a34ebb0da6
Arthur Valério <31370475+arthwrvl@users.noreply.github.com>
2023-09-19 16:15:07 -0300

    create files method

--- src/Disk.java
+++ src/Disk.java
@@ -6,6 +6,26 @@ public class Disk {
         nodes = new Node[size];
         freeSpace = size;
     }
+    public void createFile(String file){
+        if(freeSpace >= file.length()){
+            Node current = null;
+            for(int i = 0; i < file.length(); i++){
+                for(int j = 0; j < nodes.length; j++){
+                    if(nodes[j] == null){
+                        nodes[j] = new Node(file.charAt(i));
+                        freeSpace--;
+                        if(current != null){
+                            current.next = j;
+                        }
+                        current = nodes[j];
+                        break;
+                    }
+                }
+            }
+        }else{
+            System.out.println("Não há espaço livre no disco");
+        }
+    }
     public void seeFiles(){
         for(int i = 0; i < nodes.length; i++){
             if(nodes[i] != null){
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
+        Disk disk = new Disk(32);
+        disk.createFile("Arapiraca");
+        disk.createFile("Alagoas");
+        disk.seeFiles();
     }
 }
